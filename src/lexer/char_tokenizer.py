f5e90b5a2b47fcfc11eef42d15a7a2a5db0e91a3
jklypchak13 <jarod.klypchak@gmail.com>
2020-06-03 18:23:19 -0400

    Add two separate tokenizers, one for words and one for letters.

---
+++ src/lexer/char_tokenizer.py
@@ -0,0 +1,21 @@
+from lexer.tokenizer import Tokenizer
+
+
+class CharTokenizer(Tokenizer):
+    """A tokenizer that just breaks a file down by it's individual characters
+    """
+
+    token_map = {}
+
+    def __init__(self, file_path: str):
+        super().__init__(file_path)
+
+    def valid(self) -> bool:
+        """Determine if the tokenizer is still processing a single token, or if a new token has been identified.
+
+        Returns:
+            [bool] -- If a new token has been identified.
+        """
+
+        # Just check if we have large gaps of spaces, combine those into one token.
+        return self.current_char == ' ' and self.next_char == ' '
---
+++ src/lexer/word_tokenizer.py
@@ -0,0 +1,29 @@
+from lexer.tokenizer import Tokenizer
+
+
+class WordTokenizer(Tokenizer):
+    """a tokenizer breaking down a file by the different words it has.
+    """
+    separators = '.?\n\t ,![]()'
+    token_map = {}
+
+    def __init__(self, file_path: str):
+        self.separators: str = self.__class__.get_separators()
+        super().__init__(file_path)
+
+    def valid(self):
+        """Determine if the tokenizer is still processing a single token, or if a new token has been identified.
+
+        Returns:
+            [bool] -- If a new token has been identified.
+        """
+        return self.current_char != '' and (self.next_char in self.separators) == (self.current_char in self.separators)
+
+    @classmethod
+    def get_separators(cls) -> str:
+        """retrieve the list of the current separators
+
+        Returns:
+            str -- the single character separators
+        """
+        return cls.separators
