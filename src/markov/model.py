c5f06b0cb035c14363a835cddc308f0bc5273bdc
jklypchak13 <jarod.klypchak@gmail.com>
2020-06-04 02:48:06 -0400

    Added doc strings and types for the model file

--- src/markov/model.py
+++ src/markov/model.py
@@ -3,27 +3,52 @@ from lexer.ngram_generator import NGramGenerator
 import random
 
 
-def select_random(freq_map):
-    keys = list(freq_map.keys())
-    values = list(freq_map.values())
-    values = [value / sum(values) for value in values]
-    choice = random.choices(keys, values)[0]
+def select_random(freq_map: Dict[int, int]) -> int:
+    """select a random element proportionally to the given frequency map
+
+    Arguments:
+        freq_map {Dict[int, int]} -- the frequency map to select an element from
+
+    Returns:
+        int -- the selected element
+    """
+    keys: List[int] = list(freq_map.keys())
+    values: List[int] = list(freq_map.values())
+    values: list[int] = [value / sum(values) for value in values]
+    choice: int = random.choices(keys, values)[0]
     return choice
 
 
 class MarkovModel:
 
-    def __init__(self, n, tokenizer):
-        self.n = n
+    def __init__(self, n: int, tokenizer):
+        """[summary]
+
+        Arguments:
+            n {int} -- the number of tokens to utilize in one n-gram
+            tokenizer {class} -- the class of the tokenizer to use.
+        """
+        self.n: int = n
         self.data: Dict[Tuple[int], Dict[int, int]] = {}
         self.starting_values: Dict[Tuple[int], int] = {}
         self.tokenizer = tokenizer
 
-    def train(self, file_name):
-        ngrams = NGramGenerator(self.n, self.tokenizer(file_name))
+    def train(self, file_name: str) -> int:
+        """train this model with a file of text
 
-        first_ngram = ngrams.first_ngram()
-        count = 1
+        Arguments:
+            file_name {str} -- the name of the file to train this model with
+
+        Returns:
+            int -- the number of n grams in this file
+        """
+        ngrams: NGramGenerator = NGramGenerator(
+            self.n, self.tokenizer(file_name))
+
+        first_ngram: Tuple[int] = ngrams.first_ngram()
+        count: int = 1
+
+        # Account for the first ngram as a starting point.
         if first_ngram not in self.starting_values:
             self.starting_values[first_ngram] = 1
         else:
@@ -31,6 +56,8 @@ class MarkovModel:
 
         while not ngrams.finished():
             count += 1
+
+            # Add the next NGram and it's corresponding token to our frequency map
             ngram, next_token = ngrams.get_next_ngram()
 
             if ngram not in self.data.keys():
@@ -44,11 +71,20 @@ class MarkovModel:
                 freq_map[next_token] += 1
         return count
 
-    def generate(self, n):
+    def generate(self, seq_length: int) -> List[int]:
+        """generate a random sequence of tokens based on our training data.
 
-        current_ngram = select_random(self.starting_values)
-        result = list(current_ngram)
-        for i in range(n):
+        Arguments:
+            seq_length {int} -- the length of the sequence to generate
+
+        Returns:
+            List[int] -- the generated sequence of token ids
+        """
+
+        current_ngram: Tuple[int] = select_random(self.starting_values)
+        result: List[int] = list(current_ngram)
+
+        for i in range(seq_length):
             if current_ngram not in self.data.keys():
                 print('ERROR')
                 break
