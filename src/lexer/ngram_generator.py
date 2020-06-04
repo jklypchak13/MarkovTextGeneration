44c14abb7e5b4ecbc0ff63a3748673b621e32702
jklypchak13 <jarod.klypchak@gmail.com>
2020-06-04 02:34:20 -0400

    Added doc-strings and typing for lexer submodule

--- src/lexer/ngram_generator.py
+++ src/lexer/ngram_generator.py
@@ -7,21 +7,39 @@ class NGramGenerator:
     def __init__(self, n: int, tokenizer: Tokenizer):
         self.tokenizer = tokenizer
         self.n: int = n
-        self.current_ngram = tuple(
+        self.current_ngram: Tuple[int] = tuple(
             [self.tokenizer.next_token() for i in range(n)])
-        self.first = self.current_ngram
+        self.first: Tuple[int] = self.current_ngram
 
-    def get_next_ngram(self):
-        result = self.current_ngram
-        token = self.tokenizer.next_token()
+    def get_next_ngram(self) -> Tuple[Tuple[int], int]:
+        """generate the next ngram given by the current tokenizer
 
-        self.current_ngram = list(self.current_ngram)[1:]
-        self.current_ngram.append(token)
-        self.current_ngram = tuple(self.current_ngram)
+        Returns:
+            Tuple[Tuple[int], int] -- A tuple of length 2 such that
+            return[0] = the next NGram
+            return[1] = the next token after the NGram
+        """
+        result: Tuple[int] = self.current_ngram
+        token: int = self.tokenizer.next_token()
+
+        previous_ngram: List[int] = list(self.current_ngram)[1:]
+        previous_ngram.append(token)
+        self.current_ngram = tuple(previous_ngram)
+
         return result, token
 
-    def finished(self):
+    def finished(self) -> bool:
+        """Determine if this file is out of Ngrams
+
+        Returns:
+            bool -- whether this generator is done
+        """
         return self.tokenizer.end()
 
-    def first_ngram(self):
+    def first_ngram(self) -> Tuple[int]:
+        """return the first ngram encountered by this
+
+        Returns:
+            Tuple[int] -- the first ngram
+        """
         return self.first
